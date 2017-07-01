ab7c6657121f4c04017c7267018a12cedeeb4db0
Andrej Lamov <andrej.lamov@seal-software.com>
2017-07-01 16:58:08 +0200

    Move event source definitions to its namespace

--- src/food/eventsource.cljc
+++ src/food/eventsource.cljc
@@ -1,10 +1,32 @@
-(ns food.eventsource)
+(ns food.eventsource
+  (:require [food.types :as t]
+            [food.macros :as m]))
+
+(defn flatall [& args]
+  (->
+   (map (fn [d] [d]) args)
+   (flatten)))
+
+(defmulti fold (fn [acc d] (m/get-type d)))
+(defmethod fold :CreateList [acc d]
+  (let [list-name (t/CreateList-title d)]
+    (assoc acc list-name (t/List list-name []))))
+(defmethod fold :AddItem [acc d]
+  (let [path (flatall (t/AddItem-list-name d) (t/List-items--path))
+        item (t/AddItem-item d)]
+    (update-in acc
+               path
+               conj
+               item)))
 
 (defn leave-trail
   [fun acc value]
   (conj acc (fun (last acc) value)))
 
-(defn trail-reduce [fun initial vec]
-  (reduce (partial leave-trail fun)
-          [initial]
-          vec))
+(defn trail-reduce
+  ([fun initial vec]
+   (reduce (partial leave-trail fun)
+           [initial]
+           vec))
+  ([initial vec]
+   (trail-reduce fold initial vec)))
--- test/food/eventsource_test.clj
+++ test/food/eventsource_test.clj
@@ -8,25 +8,6 @@
   (t/is (= [0 1 2 3 5]
            (sut/trail-reduce + 0  [1 1 1 2]))))
 
-(defn flatall [& args]
-  (->
-   (map (fn [d] [d]) args)
-   (flatten)))
-
-(defmulti fold (fn [acc d] (get-type d)))
-
-(defmethod fold :CreateList [acc d]
-  (let [list-name (CreateList-title d)]
-    (assoc acc list-name (List list-name []))))
-
-(defmethod fold :AddItem [acc d]
-  (let [path (flatall (AddItem-list-name d) (List-items--path))
-        item (AddItem-item d)]
-    (update-in acc
-               path
-               conj
-               item)))
-
 (t/deftest reduce-with-types
   (let [events [(CreateList "my list")
 
@@ -47,8 +28,8 @@
             {"my list" (List "my list" [(Item "apple" "apple.jpg")
                                         (Item "orange" "orange.jpg")])}]
 
-           (sut/trail-reduce fold {} events)))))
+           (sut/trail-reduce {} events)))))
 
 (t/deftest flatten-everything
   (t/is (= [1 2 3]
-           (flatall 1 [2] [[[3]]]))))
+           (sut/flatall 1 [2] [[[3]]]))))
