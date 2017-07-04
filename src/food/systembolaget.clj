829e874f7b40d9fb1abb8d82ad12e2f7261d9824
Alexander Wingård <alexander.wingard@gmail.com>
2017-07-04 03:00:14 +0200

    Continue working on bolaget

--- src/food/systembolaget.clj
+++ src/food/systembolaget.clj
@@ -1,6 +1,18 @@
-(ns food.systembolaget)
+(ns food.systembolaget
+  (:require [clojure.data.json :as json]))
 
 
 (defn build-url [search-text]
   (let [url "https://www.systembolaget.se/api/productsearch/search/sok-dryck/?sortdirection=Ascending&site=all&fullassortment=0&searchquery="]
     (str url search-text)))
+
+(defn transform [data]
+  data)
+
+(defn parse [data]
+  (get (json/read-str data) "ProductSearchResults"))
+
+(defn parse-and-transform [fetched-data]
+  (-> fetched-data
+      (parse)
+      (transform)))
--- test/food/systembolaget_test.clj
+++ test/food/systembolaget_test.clj
@@ -1,7 +1,14 @@
 (ns food.systembolaget-test
   (:require [food.systembolaget :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [food.util :as util]))
 
 (t/deftest build-url-test
-  (t/is (= "https://www.systembolaget.se/api/productsearch/search/sok-dryck/?searchquery=renat&sortdirection=Ascending&site=all&fullassortment=0"
+  (t/is (= "https://www.systembolaget.se/api/productsearch/search/sok-dryck/?sortdirection=Ascending&site=all&fullassortment=0&searchquery=renat"
            (sut/build-url "renat"))))
+
+(t/deftest json-parse-test
+  (let [result (->> "resources/test/systembolaget-renat-search-result.json"
+                    (util/get-data :file)
+    -transform))]
+    (t/is (= #{} result))))
