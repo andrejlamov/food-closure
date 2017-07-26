9036625c753e6ec55eddbb79025c09e67b4b37c4
Andrej Lamov <andrej.lamov@gmail.com>
2017-07-26 23:20:26 +0200

    Save client state

--- src/food/client.cljs
+++ src/food/client.cljs
@@ -9,11 +9,14 @@
 
 (def ws (js/WebSocket. "ws://localhost:3449/ws"))
 
-(def data (atom []))
+(def client-data (atom {"sidebar-is-visible" true}))
+(def server-data (atom []))
 
-(defn log [& args]
-  (println "*** cljs: " args)
-  args)
+(add-watch
+ client-data :watcher
+ (fn [_key _atom _old-state new-state]
+   (println "*** client state")
+   (println new-state)))
 
 (defn destruct [component]
   (let [[tag & other] component]
@@ -21,10 +24,16 @@
       [tag (apply hash-map other) []]
       [tag (apply hash-map (butlast other)) (last other)])))
 
-(defn view [data]
-  [
-   ["div" :merge (... (attr "class" "ui top attached topbar menu"))
-    [["a" :merge (... (attr "class" "icon item toggle_sidebar"))
+(defn toggle-sidebar []
+  (.sidebar (js/$ "#app .bottom .sidebar") "toggle")
+  (swap! client-data update "sidebar-is-visible" not true))
+(defn sidebar-is-visible []
+  (get @client-data "sidebar-is-visible" false))
+
+(defn view [server-data]
+  [["div" :merge (... (attr "class" "ui top attached topbar menu"))
+    [["a" :merge (... (attr "class" "icon item toggle_sidebar")
+                      (on "click" toggle-sidebar))
       [["i" :merge (... (attr "class" "content icon"))]]]
      ["div" :merge (... (attr "class" "ui transparent icon input"))
       [["input" :merge (... (attr "type" "text")
@@ -35,40 +44,41 @@
        ["i" :merge (... (attr "class" "search icon"))]]]]]
 
    ["div" :merge (... (attr "class" "ui bottom attached segment pushable"))
-    [["div" :enter (... (attr "class" "ui left inline vertical sidebar menu"))
-      (map (fn [c] ["a"
-                   :id (t/List-name c)
-                   :onexit (...
-                            (style "transform" "scaleY(1)")
-                            transition
-                            (duration 1000)
-                            (style "height" "0px")
-                            (style "padding-top" "0")
-                            (style "padding-bottom" "0")
-                            (style "transform" "scaleY(0)"))
-                   :onenter (...
-                             (each (fn []
-                                     (this-as this
-                                       (let [self   (.. js/d3 (select this))
-                                             height (.. self (style "height"))
-                                             top    (.. self (style "padding-top"))
-                                             bottom (.. self (style "padding-bottom"))]
-                                         (.. self
-                                             (style "transform" "scaleY(0)")
-                                             (style "padding-bottom" 0)
-                                             (style "padding-top" 0)
-                                             (style "height" 0)
-                                             (transition)
-                                             (duration 1000)
-                                             (style "height" height)
-                                             (style "transform" "scaleY(1)")
-                                             (style "padding-bottom" top)
-                                             (style "padding-top" bottom)))))))
-                   :merge (... (attr "class" "item")
-                               (text (fn [] (t/List-name c))))])
-           (->> (t/Lists-lists data)))]
-     ["div"
-      :enter (... (attr "class" "pusher"))
+    [["div" :merge (... (attr "class" "ui left inline vertical sidebar menu")
+                        (classed "visible" sidebar-is-visible))
+      (for [c (t/Lists-lists server-data)]
+        ["a" :merge (... (attr "class" "item")
+                         (text (fn [] (t/List-name c))))
+         :id (t/List-name c)
+         :onexit (... (style "transform" "scaleY(1)")
+                      transition
+                      (duration 1000)
+                      (style "height" "0px")
+                      (style "padding-top" "0")
+                      (style "padding-bottom" "0")
+                      (style "transform" "scaleY(0)"))
+         :onenter (...
+                   (each (fn []
+                           (this-as this
+                                    (let [self   (.. js/d3 (select this))
+                                          height (.. self (style "height"))
+                                          top    (.. self (style "padding-top"))
+                                          bottom (.. self (style "padding-bottom"))]
+                                      (.. self (style "transform" "scaleY(0)")
+                                          (style "padding-bottom" 0)
+                                          (style "padding-top" 0)
+                                          (style "height" 0)
+                                          transition
+                                          (duration 1000)
+                                          (style "height" height)
+                                          (style "transform" "scaleY(1)")
+                                          (style "padding-bottom" top)
+                                          (style "padding-top" bottom)))))))])]
+
+     ["div" :merge (... (attr "class" "pusher")
+                        (classed "dimmed" sidebar-is-visible)
+                        (on "click" (fn [] (when (sidebar-is-visible)
+                                             (toggle-sidebar)))))
       [["div" :merge (... (attr "class" "ui basic segment"))]]]]]])
 
 (defn render [parent state]
@@ -77,9 +87,8 @@
                                          (let [nodes (js/Array.from (aget this "childNodes"))]
                                            (.filter nodes (fn [n] (.-tagName n))))))
                     (data (clj->js state) (fn [d i]
-                                            (let [[tag funs children] (destruct d)]
-                                              (println (get funs "id"))
-                                              (get funs "id" i)))))
+                                            (let [[tag {:strs [id]} children] (destruct d)]
+                                              (or id i)))))
         exited  (.. joined exit)
 
         entered  (.. joined
@@ -93,8 +102,8 @@
         (each (fn [d]
                 (this-as this
                          (let [self (.. js/d3 (select this))
-                               [tag funs children] (destruct d)
-                               onexit (or (get funs "onexit") identity)]
+                               [tag {:strs [onexit]} children] (destruct d)
+                               onexit (or onexit identity)]
                            (.. (onexit self)
                                (on "end" (fn []
                                            (this-as this
@@ -104,24 +113,24 @@
     (.. entered
         (each (fn [d i]
                 (this-as this
-                         (let [[tag funs children] (destruct d)
-                               draw   (or (get funs "merge") (get funs "enter") identity)
-                               onenter (or (get funs "onenter") identity)
+                         (let [[tag {:strs [merge enter onenter]} children] (destruct d)
+                               draw   (or merge enter identity)
+                               onenter (or onenter identity)
                                self (->> (.. js/d3 (select this))
                                          draw
                                          onenter)]
                            (render self children))))))
     (.. joined
         (each (fn [d]
-                (let [[tag funs children] (destruct d)
-                      draw  (or (get funs "merge") identity)]
+                (let [[tag {:strs [merge]} children] (destruct d)
+                      draw  (or merge identity)]
                   (this-as this
                            (->> (.. js/d3 (select this))
                                 (draw)
                                 (#(render %1 children))))))))))
 
 (add-watch
- data :watcher
+ server-data :watcher
  (fn [_key _atom _old-state new-state]
 
    (render (.. js/d3 (select "#app"))
@@ -131,7 +140,7 @@
        (sidebar (clj->js
                  {:context (js/$ "#app .ui.bottom.segment")}))
        (sidebar "setting" "transition" "overlay")
-       (sidebar "attach events" "#app .topbar .item.toggle_sidebar"))))
+       )))
 
 (defn send [d]
   (->> d
@@ -140,7 +149,6 @@
 
 (defmulti evaluate m/get-type)
 (defmethod evaluate :CandidateList [d]
-  ;; (println (first d))
   (->>
    d
    (t/CandidateList-items)
@@ -149,27 +157,17 @@
    (send)))
 
 (defmethod evaluate :Lists [new-data]
-  (reset! data new-data))
+  (reset! server-data new-data))
 
 (defn main []
-  (log "main!")
   (send (t/AllLists))
   ;; (send (t/SearchQuery "rostade mandlar" (t/Mathem)))
 )
 (set! (.-onopen ws) (fn []
                       (send (t/Subscribe))
-                      (log "open")))
+                      (main)))
 
-(set! (.-onmessage ws) (fn [data]
-                         (->> (.-data data)
+(set! (.-onmessage ws) (fn [server-data]
+                         (->> (.-data server-data)
                               (cljs.reader/read-string)
                               (evaluate))))
-
-;; (.. js/d3 (select "body")
-;;     (attr "class" "3"))
-
-;; ((...
-;;   (attr "class" "2")
-;;   (attr "style" "asd3")
-;;   ) (.. js/d3 (select "body")))
-
