eef1b9f05f60f554c7649ecbeefeaf86de78c0bd
Andrej Lamov <andrej.lamov@gmail.com>
2017-08-26 15:21:01 +0200

    Handle enter without exit

--- src/food/client.cljs
+++ src/food/client.cljs
@@ -3,7 +3,7 @@
             [cljsjs.d3]
             [cljsjs.jquery]
             [cljs.core.async :as async
-             :refer [<! >! chan close! sliding-buffer put! alts!]]
+             :refer [<! >! timeout chan close! sliding-buffer put! alts!]]
             [food.render :as r :refer [render transform]]
             [food.macros :refer [d3]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
@@ -11,31 +11,37 @@
 (enable-console-print!)
 
 (def state (atom {:top-items #{
-                               ;; "chrome"
+                               "chrome"
                                }
 
-                  :list-items #{"chrome"
+                  :list-items #{
+                                "chrome"
                                 "firefox"
                                 "safari"
                                 "edge"
-                                "opera"}}))
+                                "opera"
+                                }}))
 
 (defn pos [e]
   (let [o (.. (js/$ e) offset)]
     [(.-top o) (.-left o)]))
 
-(def input-chan (chan))
+(def input-chan (chan) )
 (def our-pub (async/pub input-chan :topic))
 
 (defn reg [topic chan]
   (async/sub our-pub topic chan))
 
-(defn rpc [topic cb]
-  (let [c (chan)]
+(defn rpc [topic on-success on-error]
+  (let [c (timeout 0)]
     (put! input-chan {:topic topic :pid c})
-    (go (cb (<! c)))))
+    (go (let [a (<! c)]
+          (if (nil? a)
+            (on-error a)
+            (on-success a))))))
 
 (declare root)
+
 (defn top-bar []
   [:div.ui.top.attached.menu {:join (d3 (style "height" "6em"))}
    (for [n (:top-items @state)]
@@ -55,40 +61,43 @@
                                          (style "width" "6em")
                                          (style "padding-left" lp)
                                          (style "padding-right" rp)
-                                         (on "end" #(rpc (str "exit/list-item/" n)
-                                                         (fn [e]
-                                                           (let [
-                                                                 i1       (.. self (select "i"))
-                                                                 i0       (.. e (select "i"))
-                                                                 [t1 l1] (pos (.. i1 node))
-                                                                 [t0 l0] (pos (.. i0 node))
-                                                                 t (- (- t1 t0))
-                                                                 l (- (- l1 l0 4 ))
-                                                                 ]
-                                                             (.. i1
-                                                                 (style "transform" (str "translate(" l "px," t "px)"))
-                                                                 (style "z-index" 1)
-                                                                 (style "opacity" 1)
-                                                                 (transition)
-                                                                 (duration 1000)
-                                                                 (style "transform" (str "translate(0,0"))
-                                                                 (on "end" (fn [] (.. e
-                                                                                     (transition)
-                                                                                     (duration 1000)
-                                                                                     (style "height" "0")
-                                                                                     (style "padding-top" "0")
-                                                                                     (style "padding-bottom" "0")
-                                                                                     remove)))
-                                                                 )
-                                                             (.. i0 (style "opacity" 0))
-                                                             ))))))))))}
+                                         (on "end"
+                                             #(rpc (str "exit/list-item/" n)
+                                                   (fn [[e that]]
+                                                     (let [i1       (.. self (select "i"))
+                                                           i0       (.. e (select "i"))
+                                                           [t1 l1] (pos (.. i1 node))
+                                                           [t0 l0] (pos (.. i0 node))
+                                                           t (- (- t1 t0))
+                                                           l (- (- l1 l0 4))]
+                                                       (.. i1
+                                                           (style "transform" (str "translate(" l "px," t "px)"))
+                                                           (style "z-index" 1)
+                                                           (style "opacity" 1)
+                                                           (transition)
+                                                           (duration 1000)
+                                                           (style "transform" (str "translate(0,0"))
+                                                           (on "end" (fn [] (.. e
+                                                                               (transition)
+                                                                               (duration 1000)
+                                                                               (style "height" "0")
+                                                                               (style "padding-top" "0")
+                                                                               (style "padding-bottom" "0")
+                                                                               remove))))
+                                                       (.. i0 (style "opacity" 0))))
+
+                                                   (fn []
+                                                     (.. self (select "i")
+                                                         (style "opacity" 1))
+                                                     )
+                                                   )
+                                             )))))))}
       [:i.huge.icon {:id n
                      :join (d3 (classed n true)
                                (style "color" "red")
                                ;; (style "opacity" 1)
-                               )
-                     :enter (d3 (style "opacity" 0))
-                     }]])])
+)
+                     :enter (d3 (style "opacity" 0))}]])])
 
 (defn item-list []
   [:div.ui.bottom.attached.segment
@@ -112,9 +121,11 @@
                   (attr "class" "ui vertical segment"))
        :exit  (d3 (each (fn [] (this-as this
                                 (let [output-chan (chan)]
+
                                   (reg (str "exit/list-item/" n) output-chan)
                                   (go (let [{:keys [pid topic]} (<! output-chan)]
-                                        (put! pid (.. js/d3 (select this))))))))))}
+                                        (println "exit" n)
+                                        (put! pid [(.. js/d3 (select this)) this]))))))))}
       [:div.ui.icon.item>i.huge.icon
        {:join (d3
                (classed n true))}]])])
